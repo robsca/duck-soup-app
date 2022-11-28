ca185f0c1302c6eaedd46cded0842ece5b687102
robsca <scalas.roberto@gmail.com>
2022-11-28 16:33:22 +0000

    Reformat in OOP - Done - Next step is connecting database

---
+++ a_NLP_Processor.py
@@ -0,0 +1,49 @@
+import datetime
+from transformers import pipeline
+
+class NLP:
+    '''
+    This class should be indipendent from the GUI
+    '''
+    def __init__(self): # works
+        print(f"Initalizing NLP_Processor class")
+        start = datetime.datetime.now()
+        # print("NLP class: Creating instance")
+        self.summarizer = pipeline("summarization")
+        #self.fill_mask = pipeline("fill-mask")
+        self.qa = pipeline("question-answering")
+        #self.classifier = pipeline("sentiment-analysis")
+        #self.ner = pipeline("ner")
+        self.text_generator = pipeline("text-generation")
+        # set pad token id to eos token id
+        self.text_generator.tokenizer.pad_token_id = self.text_generator.tokenizer.eos_token_id
+        #self.conversational = pipeline("conversational")
+        end = datetime.datetime.now()
+        # time in seconds
+        time = (end - start).total_seconds()
+        print(f"Initialization NLP_Processor completed: {time} seconds")
+
+    def summarize(self, prompt, max_length=100, min_length=30, delete_prompt=True): # works
+        print("Generating summary") 
+        # delete last word from prompt
+        last_word = prompt.split()[-1]
+        prompt = prompt.replace(last_word, "")
+        summary = self.summarizer(prompt, max_length=max_length, min_length=min_length, truncation=True)
+        summary = summary[0]['summary_text']
+        return summary
+    
+    def generate_text(self, prompt, num = 100): # works
+        print(f"Generating {num} words")
+        len_prompt = len(prompt.split())
+        last_word = prompt.split()[-1]
+        # delete last word from prompt
+        prompt = prompt.replace(last_word, "")
+        # generate text
+        generated = self.text_generator(prompt, max_length=len_prompt+num, do_sample=True, top_k=50, top_p=0.95, num_return_sequences=1)
+        generated = generated[0]['generated_text']
+        return generated
+
+    def answer_question(self, prompt, question):
+        answer = self.qa(question=question, context=prompt)
+        return answer['answer']
+    
---
+++ c_Silver_Scraper.py
@@ -0,0 +1,119 @@
+import requests
+from bs4 import BeautifulSoup
+
+class Silver_Scraper:
+    def __init__(self, url):
+        self.url = url
+    # 1. WIKI
+    def get_wiki_text(self, chapter=None):
+        page = requests.get(self.url) # get the page
+        soup = BeautifulSoup(page.content, 'html.parser') # create a BeautifulSoup object
+        chapters = soup.find_all('h2') # find all the chapters
+        chapters = [c.get_text() for c in chapters] # get the text of the chapters
+
+        # print(chapters)
+        
+        if chapter in chapters:
+            print(f"Getting chapter {chapter}")
+            # iterate over the text if find p or h2
+            text = []
+            for t in soup.find_all(['p', 'h2']):
+                if t.name == 'h2': # if the tag is h2
+                    if t.get_text() == chapter: # if the text is the chapter
+                        print("Found chapter")
+                        text.append(t.get_text()) # append the text
+                        next_p = t.find_next('p') # find the next p
+                        text.append(next_p.get_text()) # append the text
+                        next_h2 = t.find_next('h2') # find the next h2
+                        # all the text until the next_2
+                        for t in t.find_next_siblings(): # iterate over the siblings
+                            if t == next_h2:
+                                break
+                            else:
+                                text.append(t.get_text())
+                    else:
+                        # handle the case where the chapter is not found
+                        pass
+
+            # join the text
+            text = ' '.join(text)
+            return text, chapters
+        else:
+            # find the text from p and h2
+            text = [t.get_text() for t in soup.find_all(['p', 'h2'])]
+            # join the text
+            text = ' '.join(text)
+            return text, chapters
+
+    def scraping_wiki(self, last_word):
+        print("Getting wikipedia summary")
+        word = last_word[6:] # get the word to search
+        if "+" in word:
+            word_ = word.split("+")[0] 
+            chapter_ = word.split("+")[1]
+            print(f"Getting chapter {chapter_} of {word_}")
+            url = f"https://en.wikipedia.org/wiki/{word_}"
+            text, chapters = self.get_wiki_text(url, chapter_)
+            # replace the / with a space
+            wiki_text = text.replace("/", " ")
+            return wiki_text
+        else:
+            chapter_ = None
+            url = f"https://en.wikipedia.org/wiki/{word}"
+            text, chapters = self.get_wiki_text(url)
+            # replace the / with a space
+            text = text.replace("/", " ")
+            return text, chapters
+    # 2. URL
+    def get_text_from_url(self, chapter=None):
+        page = requests.get(self.url) # get the page
+        soup = BeautifulSoup(page.content, 'html.parser') # create a BeautifulSoup object
+        chapters = soup.find_all('h2') # find all the chapters
+        chapters = [c.get_text() for c in chapters] # get the text of the chapters
+
+        # print(chapters)
+        
+        if chapter in chapters:
+            print(f"Getting chapter {chapter}")
+            # iterate over the text if find p or h2
+            text = []
+            for t in soup.find_all(['p', 'h2']):
+                if t.name == 'h2': # if the tag is h2
+                    if t.get_text() == chapter: # if the text is the chapter
+                        print("Found chapter")
+                        text.append(t.get_text()) # append the text
+                        next_p = t.find_next('p') # find the next p
+                        text.append(next_p.get_text()) # append the text
+                        next_h2 = t.find_next('h2') # find the next h2
+                        # all the text until the next_2
+                        for t in t.find_next_siblings(): # iterate over the siblings
+                            if t == next_h2:
+                                break
+                            else:
+                                text.append(t.get_text())
+                    else:
+                        # handle the case where the chapter is not found
+                        pass
+
+            # join the text
+            text = ' '.join(text)
+            return text, chapters
+        else:
+            # find the text from p and h2
+            text = [t.get_text() for t in soup.find_all(['p', 'h2'])]
+            # join the text
+            text = ' '.join(text)
+            return text, chapters
+
+    def scraping_url(self):
+        print("Getting wikipedia summary")
+        text, chapters = self.get_text_from_url()
+        # replace the / with a space
+        text = text.replace("/", " ")
+        return text, chapters
+            
+if __name__ == "__main__":
+    url = "https://en.wikipedia.org/wiki/Python_(programming_language)"
+    scraper = Silver_Scraper(url)
+    text, chapters = scraper.get_wiki_text("History")
+    print(text)
