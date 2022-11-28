9e40a875cac05644e304b32a554eaa1132f26e1a
robsca <scalas.roberto@gmail.com>
2022-11-28 19:57:36 +0000

    create save function - need more work

--- d_Note_App.py
+++ d_Note_App.py
@@ -27,12 +27,23 @@ class NoteApp:
         delete_button = tk.Button(self.root, text="Hide", command=lambda: delete_listbox())
         delete_button.grid(row=1, column=8, sticky="nsew", padx=5, pady=5)
 
+    def save_note(self):
+        # get the title from the text editor
+        title = self.text_editor.title_entry.get()
+        # get the text from the text editor
+        text = self.text_editor.get_text()
+        # save the note
+        self.database.new_note(title, text, 'date', 'tags')
+        
     def write(self):
         # create a button to 
         button_all_notes = tk.Button(self.root, text="All Notes", command=lambda: self.list_of_notes())
         button_all_notes.grid(row=2, column=8, sticky="nsew", padx=5, pady=5)
         self.text_editor = TextEditor(self.root)
         self.text_editor.text_entry.focus()
+        # create a button to save the note
+        button_save = tk.Button(self.root, text="Save", command=lambda: self.save_note())
+        button_save.grid(row=3, column=8, sticky="nsew", padx=5, pady=5)
 
     def run(self):
         self.root.mainloop()
